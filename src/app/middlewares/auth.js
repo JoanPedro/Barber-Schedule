e89afb36832cc2b827f91fa310220fe723dcb22e
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-14 20:08:35 -0300

    Define o Middleware AUTH para Edição de Usuários

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,36 @@
+// Middleware de autenticação para verificar se o usuário está autenticado.
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+// Local onde está o segredo do Token.
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization; // Adquire o Bearer Token
+
+  // Verifica se o token foi passado para requisição via Header (Bearer).
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided!' });
+  }
+
+  /* O Header retorna uma string: "Beader asfhaopsifhiaoephai", utilizar o split
+  para retornar um array com 2 informações: Bearer e a outra parte do Token.
+  Como não será utilizado o bearer, somente o [, token] basta. */
+  const /* [bearer, token] */ [, token] = authHeader.split(' ');
+
+  try {
+    /* Como o jwt.verify possui um padrão antigo de Callback, utiliza-se o
+    promisify para fazer um parse de callback para promise e utilizar
+    o Async await */
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    /* Se for decodificado com sucesso, como o SessionController passou o ID do
+    usuário pro jwt.sign, o decoded  vai possuir dentro dele (Objeto) o ID
+    passado. */
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token invalid!' });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,19 @@ import { Router } from 'express'; // Importa o padrão Router do Express.
 import UserController from './app/controllers/UserController';
 // Importa o Controller de sessão.
 import SessionController from './app/controllers/SessionController';
+// Importa o Middleware de Autorização
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router(); // Cria uma nova instância de Router do Express.
 
-// Registra o Middleware de Cadastro de Usuário -> UserController.
+// Registra o Controller de Cadastro de Usuário (store) -> UserController.
 routes.post('/users', UserController.store);
 
-// Registra o Middleware para fazer o Sign in na aplicação -> SessionController.
+// Registra o Controller de Atualização de dados (update) -> UserController.
+// Rota só permitida para usuários autenticados -> authMiddleware.
+routes.put('/users', authMiddleware, UserController.update);
+
+// Registra o Controller para fazer o Sign in na aplicação -> SessionController.
 routes.post('/sessions', SessionController.store);
 
 export default routes;

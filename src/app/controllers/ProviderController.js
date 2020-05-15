09b927d433000fc76b38da6eb2dfc055efbee86c
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-15 17:38:13 -0300

    Lista Prestadores de Serviços mais Relacionamento

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,21 @@
+import User from '../models/User';
+import File from '../models/File';
+
+class ProviderController {
+  async index(req, res) {
+    const providers = await User.findAll({
+      // Retorna todos os usuários onde provider = true
+      where: { provider: true },
+
+      // Retorna somente estas informações listadas.
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+
+      // Inclue o retono das informações do Relacionamento com o model File.
+      include: [{ model: File }],
+    });
+
+    return res.json(providers);
+  }
+}
+
+export default new ProviderController();
--- src/routes.js
+++ src/routes.js
@@ -10,21 +10,33 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 // Importa o Controller de Upload de arquivos.
 import FileController from './app/controllers/FileController';
+// Importa o Controller de Prestadores de Serviços.
+// OBS. Apesar de ser também um usuário, a entidade é outra.
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router(); // Cria uma nova instância de Router do Express.
 const upload = multer(multerConfig);
 
+// ------------------------ Usuário ------------------------------------------ //
 // Registra o Controller de Cadastro de Usuário (store) -> UserController.
 routes.post('/users', UserController.store);
 
 // Registra o Controller de Atualização de dados (update) -> UserController.
 // Rota só permitida para usuários autenticados -> authMiddleware.
 routes.put('/users', authMiddleware, UserController.update);
+// --------------------------------------------------------------------------- //
 
+// ------------------------ Sessão de Autenticação --------------------------- //
 // Registra o Controller para fazer o Sign in na aplicação -> SessionController.
 routes.post('/sessions', SessionController.store);
+// --------------------------------------------------------------------------- //
 
-// Rota para Upload de arquivos.
+// ------------------------- Rota Upload de Arquivos ------------------------- //
+// Rota para Upload de arquivos, com Controller (store) -> FileController.
 routes.post('/files', upload.single('file'), FileController.store);
+// --------------------------------------------------------------------------- //
 
+// ------------------------- Prestadores de Serviço -------------------------- //
+// Regista o Controller de Prestadores de Serviço () -> ProviderController.
+routes.get('/providers', ProviderController.index);
 export default routes;

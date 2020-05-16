20a5cf9d41e2e78f46ddf446ceec6c7d9d8ac0e3
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-15 21:57:07 -0300

    Validação de agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -27,12 +28,34 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers.' });
     }
 
+    // Sempre pega o início da hora e formata a string de data para o formato ISO.
+    // Se: 19:30:00 -> 19:00:00!
+    const hourStart = startOfHour(parseISO(date));
+
+    // Verifica se a data passada pelo usuário é anterior à data atual.
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past datas are not permitted.' });
+    }
+
+    // Varifica a disponibilidade de horário.
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id, // provider_id: provider_id.
+        canceled_at: null, // Se o agendamento for cancelado, data estará disponível.
+        date: hourStart, // Não permite horário quebrado. Sempre de 1 em 1 hora.
+      },
+    });
+
+    // Horário não disponível.
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Appointment is not available' });
+    }
     // Cria os agendamentos.
     const appointment = await Appointment.create({
       // Setado automaticamente pelo middleware de auteticação (auth -> userId).
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart, // Não permite horário quebrado. Sempre de 1 em 1 hora.
     });
 
     return res.json(appointment);

42184445e52025f01d7b9e7151352c563045c360
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-17 12:55:51 -0300

    Configura Nodemailer para envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,18 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'dac93705f329bd',
+    pass: '7d28956816cfc7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril - Só Mailchimp
+// Mailtrap (DEV)

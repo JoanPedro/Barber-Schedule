bbae822d2e060b15f704aa264ea28ef612441ad9
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-18 13:24:21 -0300

    Sentry e Youch para tratamento de excessões

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
 import express from 'express'; // Só é possível utilizar isso adicionando como
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+// Deve ser importado antes das rotas. Para entrar no middleware.
+import 'express-async-errors';
 import routes from './routes'; // dependencias o sucrase. " Yarn add sucrase -D "
 // Deve-se criar o arquivo nodemon.js e configuralo para aceitar JS sucrase!
 // Configuração Inicial da Aplicação: Define os Middlewares e Rotas.
 // Padrão para configuração de: Testes unitários e de Integração.
 import './database';
+import sentryConfig from './config/sentry';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json()); // Habilita Requisição Body em JSON.
 
     /* Estático: Servem arquivos estáticos como html, imagens... arquivos
@@ -27,6 +36,17 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  // Middleware para tratamento de excessões.
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      // Erro de servidor.
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  dsn:
+    'https://6d4c2a1225cf46d4922813689d6af881@o394603.ingest.sentry.io/5244943',
+};
